febae765e063bee6c031a40fa6249d753e0612b7
sanderblue <william.a.blue@gmail.com>
2013-09-08 12:42:11 -0500

    add new Angular-Express-MySQL project to work section

--- web/assets/js/graphs.js
+++ web/assets/js/graphs.js
@@ -36,6 +36,18 @@ $(document).ready(function() {
         }
     });
 
+    $('.pop-angularexpress').popover({
+        html: true,
+        content: $('#project-angularexpress').html(),
+        placement: function (popover_element) {
+            if ($(window).width() < 768) {
+                return 'bottom';
+            } else {
+                return 'right';
+            }
+        }
+    });
+
     $('.pop-gitgraph').on('click', function () {
 
         $("#chartgitgraph").gitGraph({
@@ -48,7 +60,7 @@ $(document).ready(function() {
             $('.pop-gitgraph').popover('toggle');
         });
 
-        $('.pop-polymath, .pop-api').popover('hide');
+        $('.pop-polymath, .pop-api, .pop-angularexpress').popover('hide');
     });
 
     $('.pop-polymath').on('click', function () {
@@ -63,7 +75,7 @@ $(document).ready(function() {
             $('.pop-polymath').popover('toggle');
         });
 
-        $('.pop-gitgraph, .pop-api').popover('hide');
+        $('.pop-gitgraph, .pop-api, .pop-angularexpress').popover('hide');
     });
 
     $('.pop-api').on('click', function () {
@@ -78,6 +90,21 @@ $(document).ready(function() {
             $('.pop-api').popover('toggle');
         });
 
-        $('.pop-gitgraph, .pop-polymath').popover('hide');
+        $('.pop-gitgraph, .pop-polymath, .pop-angularexpress').popover('hide');
+    });
+
+    $('.pop-angularexpress').on('click', function () {
+
+        $("#chartangularexpress").gitGraph({
+            html: 'chartangularexpress',
+            user: 'sanderblue',
+            repo: 'angular-express-mysql'
+        });
+
+        $('.close-popover').on('click', function () {
+            $('.pop-angularexpress').popover('toggle');
+        });
+
+        $('.pop-gitgraph, .pop-polymath, .pop-api').popover('hide');
     });
 });
